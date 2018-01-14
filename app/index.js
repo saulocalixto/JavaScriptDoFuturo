92b686d93ab3251b3b891da959a012d634ef7879
Saulo Calixto <sauloacalixto@gmail.com>
2018-01-14 08:55:52 -0200

    Treinando HotModuleReplacement.

--- app/index.js
+++ app/index.js
@@ -2,3 +2,12 @@ import Message from './model/message.model';
 
 console.log('Index started');
 console.dir(new Message());
+
+document.getElementById('send').onclick = () => {
+  const m = new Message(document.getElementById('message').value);
+  document.getElementById('messages').innerHTML += `<li>${m.text} ${m.created}</li>`;
+};
+
+if(module && module.hot) {
+  module.hot.accept();
+}
